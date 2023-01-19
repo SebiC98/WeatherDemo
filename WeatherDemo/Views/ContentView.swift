207fe4f35a47d74caaba544bfa4e95c965ad09ea
SebiC98 <sebastiancioata98@gmail.com>
2023-01-19 13:49:36 +0200

    Get Coordinations

---
+++ WeatherDemo/Views/ContentView.swift
@@ -0,0 +1,38 @@
+//
+//  ContentView.swift
+//  WeatherDemo
+//
+//  Created by Sebastian Cioată on 19.01.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @StateObject var locationManager = LocationManager()
+    
+    var body: some View {
+        VStack {
+            
+            if let location = locationManager.location{
+                Text("Your coordinate are: \(location.longitude), \(location.latitude)")
+            }else{
+                if locationManager.isLoading{
+                    LoadingView()
+                }else{
+                    WelcomeView()
+                         .environmentObject(locationManager)
+                }
+            }
+           
+        }
+        .background(Color(hue: 0.656, saturation: 0.787, brightness: 0.354))
+        .preferredColorScheme(.dark)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
