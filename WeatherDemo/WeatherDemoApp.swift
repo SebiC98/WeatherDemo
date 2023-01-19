78739e4e00195fcec25a64d215c767a2ebb601e5
SebiC98 <sebastiancioata98@gmail.com>
2023-01-19 13:32:09 +0200

    Base Project

---
+++ WeatherDemo/WeatherDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDemoApp.swift
+//  WeatherDemo
+//
+//  Created by Sebastian Cioată on 19.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
